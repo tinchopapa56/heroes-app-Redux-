641b8046f3068f04d2d870c0b969628f384e781b
Tincho develops <martinlabastie.p@gmail.com>
2022-08-19 12:38:08 -0300

    router implemented

---
+++ src/components/ShowTeam/ShowTeam.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+function ShowTeam() {
+  return (
+    <div>
+        ShowTeam
+        <Link to="/"><h2> back to home </h2></Link>
+    </div>
+  )
+}
+
+export default ShowTeam
